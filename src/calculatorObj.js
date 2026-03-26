cf5629ce58494bc40a8526bd6aa13290e628c57d
August Johnson <gurojohnson@gmail.com>
2026-03-26 15:45:04 -0400

    Create calculator object with math function functions and test each

---
+++ src/calculatorObj.js
@@ -0,0 +1,15 @@
+export const calculator = {
+    sum: function (a, b) {
+        return a + b;
+    },
+    subtract: function (a, b) {
+        return a - b;
+    },
+    divide: function (a, b) {
+        return a / b;
+    },
+    multiply: function (a, b) {
+        return a * b;
+    }
+};
+
